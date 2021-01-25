c9feb30683d6c2b15830b1ec8404898c6771f96a
Mugisha Thierry <thierrymugis@gmail.com>
2021-01-25 15:41:07 +0200

    Added profiles form

--- rating/forms.py
+++ rating/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Project, Profile, Rate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -8,3 +9,18 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['first_name','second_name','phone', 'bio','profile_pic']
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ['title','project_pic','description','project_link']
+
+class RateForm(forms.ModelForm):
+    class Meta:
+        model = Rate
+        exclude = ['user', 'project']        
